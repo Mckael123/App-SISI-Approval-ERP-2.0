9aa9657010f8970e2d061003bbb82498d3c93fe2
McKael-forti <76393985+Mckael123@users.noreply.github.com>
2022-02-07 11:59:32 +0700

    first

---
+++ app/src/main/java/com/example/forcaerp20/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.forcaerp20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        supportActionBar?.hide()//ngilangin bar atas
+    }
+}
