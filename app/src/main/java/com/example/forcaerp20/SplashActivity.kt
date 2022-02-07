323292f110ce525f3fe340a7af9fd164cbb195ab
ArlPrima <81967164+ArlPrima@users.noreply.github.com>
2022-02-07 23:10:09 +0700

    splash
    progres 1

--- app/src/main/java/com/example/forcaerp20/SplashActivity.kt
+++ app/src/main/java/com/example/forcaerp20/SplashActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
-class SplashActivity : AppCompatActivity() {
+class  SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
